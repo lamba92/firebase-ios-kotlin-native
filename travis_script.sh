d7efccfdcc2a03d4f1a9457ffa58bc283d19f7aa
Lamba92 <basti.lamberto@gmail.com>
2020-01-03 01:31:26 +0100

    forced Gradle plain console for Travis builds

--- travis_script.sh
+++ travis_script.sh
@@ -1,4 +1,4 @@
-sudo ./gradlew extractFirebaseIosZip
+sudo ./gradlew extractFirebaseIosZip -Dorg.gradle.console=plain
 sudo chmod -R a+rwx .
 sudo chown -R travis .
-./gradlew publishToMavenLocal --parallel --console=plain
+./gradlew publishToMavenLocal --parallel -Dorg.gradle.console=plain
