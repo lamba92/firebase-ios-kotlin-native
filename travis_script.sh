36db19f07253d95141c928ae38495fd41418575a
Lamba92 <basti.lamberto@gmail.com>
2020-01-07 02:30:44 +0100

    building and publishing non static version as well

--- travis_script.sh
+++ travis_script.sh
@@ -1,4 +1,4 @@
-sudo ./gradlew extractFirebaseIosZip -Dorg.gradle.console=plain -PskipBuild=true
+sudo ./gradlew extractFirebaseIosZip --console=plain -PskipBuild=true
 sudo chmod -R a+rwx .
 sudo chown -R travis .
-./gradlew build -Dorg.gradle.console=plain
+./gradlew build --console=plain --parallel
